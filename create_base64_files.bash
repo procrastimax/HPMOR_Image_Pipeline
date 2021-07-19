14fd9068cbd0ce8cc2c8db2a602d3ed6fe036e04
procrastimax <mmeheykeroth@protonmail.com>
2021-07-19 11:19:30 +0200

    Adds script to convert all png files to base64 text files in HPMOR_data folder

---
+++ create_base64_files.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+shopt -s globstar
+cd HPMOR_data/paragraph_images/ || { echo "Cannot find folder: HPMOR_data/paragraph_images/!"; exit 1; }
+
+for f in **/*.png; do
+    base64 "$f" >> "${f:0:${#f}-4}_b64.txt"
+done
