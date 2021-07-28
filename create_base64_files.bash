c644e590323c7ddbdf0e26c62418f26162396d85
procrastimax <mmeheykeroth@protonmail.com>
2021-07-28 11:46:54 +0200

    Adds base64 prefix string to generated b64 files

--- create_base64_files.bash
+++ create_base64_files.bash
@@ -4,5 +4,5 @@ shopt -s globstar
 cd HPMOR_data/paragraph_images/ || { echo "Cannot find folder: HPMOR_data/paragraph_images/!"; exit 1; }
 
 for f in **/*.png; do
-    base64 "$f" >> "${f:0:${#f}-4}_b64.txt"
+    { echo "data:image/png;base64, "; base64 "$f"; } > "${f:0:${#f}-4}_b64.txt"
 done
